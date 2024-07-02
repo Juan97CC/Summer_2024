cedfb9b3560549e597041366255d6407e1ba27c3
Juan <consuegra_jcc@hotmail.com>
2024-07-01 21:03:16 -0400

    Search Algorithms added

---
+++ Searching_Algorithms/NaiveStringSearch.js
@@ -0,0 +1,28 @@
+
+function naiveSearch(long, short){
+    let totalCount = 0;
+    for (let i = 0; i < long.length; i++) {
+        let longIndex = i;
+        let count = 0;
+
+        for (let j = 0; j < short.length; j++) {
+            //console.log(long[i+j] + " " + short[j])
+
+
+            if (long[i+j] !== short[j]){
+                break;
+            }
+            count++;
+            if (count === short.length) totalCount++;
+
+
+
+
+
+        }
+
+    }
+    return totalCount;
+}
+
+console.log(naiveSearch("lorilollole loled", "lol"))
